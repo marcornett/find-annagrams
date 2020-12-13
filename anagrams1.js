b3d2133a3256236392f1f367fde21045641fb87d
Marcel Cornett <marcornett@gmail.com>
2020-12-13 13:27:21 -0500

    Removed comments and spacing.

--- anagrams1.js
+++ anagrams1.js
@@ -1,18 +1,14 @@
-// Alphabetize function
 function alphabetize(a) {
     return a.toLowerCase().split("").sort().join("").trim();
 }
 
-// Anagram function
 const button = document.getElementById("findButton");
+// Finds anagrams of input word
 button.onclick = function () {
-    // Finds value of input
     let typedText = document.getElementById("input").value;
-    // Alphabetize input
     let alphaText = alphabetize(typedText)
-    for (i = 0; i < words.length; i++){
-        // If text alphabetized input is equal to alphabetized words
-        if(alphaText === alphabetize(words[i])){
+    for (i = 0; i < words.length; i++) {
+        if (alphaText === alphabetize(words[i])) {
             let container = document.createElement('span')
             let text = document.createTextNode(' ' + words[i] + ' ')
             let div = document.querySelector('div')
